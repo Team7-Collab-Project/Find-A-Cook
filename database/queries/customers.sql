11dfcbbc06573e43ea62708b90e234ff72b7f4f4
Temi Owolabi <65958151+temiowolabi@users.noreply.github.com>
2022-12-08 19:16:08 +0000

    feat: created database, customer table and inserted sample data
    Refers to Scrumwise Items: 43544 & 43545

---
+++ database/queries/customers.sql
@@ -0,0 +1,40 @@
+CREATE TABLE customers(
+id BIGSERIAL NOT NULL PRIMARY KEY,
+firstname VARCHAR(50) NOT NULL,
+surname VARCHAR(50) NOT NULL,
+address VARCHAR(50) NOT NULL,
+contact_number VARCHAR(15) NOT NULL,
+email VARCHAR(150) NOT NULL,
+password VARCHAR(50) NOT NULL
+)
+
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Agata', 'Worters', '945 Hallows Pass', '211-504-1571', 'aworters0@cam.ac.uk', 'Wd5z3J7');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Masha', 'Assender', '510 Haas Park', '785-281-6818', 'massender1@flickr.com', 'KORM83b1ONp');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Morgan', 'Pavett', '53223 Maryland Avenue', '460-320-0373', 'mpavett2@twitpic.com', 'DZD7uU2');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Hieronymus', 'Lamberton', '8 Maywood Lane', '624-174-8906', 'hlamberton3@fc2.com', 'KxAWSMC');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Delora', 'Tivenan', '9441 Nevada Parkway', '570-205-0639', 'dtivenan4@odnoklassniki.ru', 'Srv4L2k3Xrb3');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Betta', 'Vear', '0945 Superior Junction', '975-705-0404', 'bvear5@goo.gl', 'EvdU1196XGY');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Ronny', 'Vellender', '273 Straubel Terrace', '624-737-5634', 'rvellender6@elpais.com', '8S2eHBQ');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Vikky', 'Menichi', '95 Huxley Pass', '962-987-2751', 'vmenichi7@instagram.com', 'tmfhS0F');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Arlyne', 'Abrahmer', '142 Thierer Terrace', '636-200-8161', 'aabrahmer8@lulu.com', '9h4siFHN');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Latrena', 'Strickler', '5 Coleman Pass', '327-430-2730', 'lstrickler9@hud.gov', 'FTWr35J7');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Maureen', 'Kelloch', '50 Cody Junction', '433-454-5669', 'mkellocha@ox.ac.uk', 'CQy0blbagB');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Darsey', 'Karpushkin', '8 Steensland Place', '327-728-4704', 'dkarpushkinb@fc2.com', 'puhIO7Xebf');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Cynde', 'Gaskoin', '798 Mifflin Alley', '117-200-7920', 'cgaskoinc@telegraph.co.uk', 'mC8Oq3jJNAsK');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Giles', 'Gye', '7113 Hoard Pass', '288-414-2814', 'ggyed@quantcast.com', 'Tb0edEmEa');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Ramsay', 'Layhe', '79 Petterle Circle', '420-221-8249', 'rlayhee@npr.org', 'rs5GDKhK');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Marisa', 'Petrollo', '59 Hudson Drive', '510-115-6411', 'mpetrollof@pen.io', 'HYLmZgfB24');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Haleigh', 'Flowers', '0604 La Follette Lane', '998-828-6000', 'hflowersg@gnu.org', 'L9mswR');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Arlette', 'Balkwill', '88027 Welch Center', '435-126-3903', 'abalkwillh@scientificamerican.com', 'Yy79lh');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Noach', 'Oughtright', '840 Killdeer Trail', '714-461-0612', 'noughtrighti@mayoclinic.com', 'nswc1hfO3p4b');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Suzette', 'Radnedge', '5 Debs Court', '782-726-9446', 'sradnedgej@hatena.ne.jp', 'xkb9wr');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Maje', 'McGettigan', '726 Scofield Center', '154-350-4583', 'mmcgettigank@amazon.co.uk', 'Nm3UWnsbVT8q');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Clara', 'Pennycuick', '43 Harbort Circle', '359-576-6231', 'cpennycuickl@illinois.edu', 'nTA9rZIy');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Jeniffer', 'Chatterton', '3340 Mitchell Place', '226-384-1325', 'jchattertonm@live.com', 'AdDwtVbwHa');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Berkie', 'Yurenev', '03 Blaine Way', '910-786-1984', 'byurenevn@nature.com', '7hx0wB');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Wildon', 'Enocksson', '62 Harper Crossing', '436-661-5790', 'wenockssono@princeton.edu', 'Zs0ErVB');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Hobart', 'Glencros', '073 Talisman Terrace', '115-398-0432', 'hglencrosp@nih.gov', 'nM52R2G9Lw0');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Aurie', 'Alpes', '2 Fulton Park', '467-809-9042', 'aalpesq@shop-pro.jp', 'uZyvU8');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Idaline', 'Fairbank', '687 Northfield Terrace', '450-644-2442', 'ifairbankr@nyu.edu', 'DfhbgfbK4r');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Ally', 'Latey', '8 Fairview Avenue', '662-982-8567', 'alateys@bloomberg.com', 'rTwEfsAr');
+insert into customers (firstname, surname, address, contact_number, email, password) values ('Teresa', 'McKinley', '60 Roth Court', '396-765-8889', 'tmckinleyt@wikia.com', 'ATqImLWgC');
