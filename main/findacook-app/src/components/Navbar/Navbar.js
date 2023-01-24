f4fbee36dd9d8e7c545ca496359fdfb672b4a29d
Temi Owolabi <65958151+temiowolabi@users.noreply.github.com>
2023-01-24 01:08:42 +0000

    Update Navbar styles

--- main/findacook-app/src/components/Navbar/Navbar.js
+++ main/findacook-app/src/components/Navbar/Navbar.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import '../CSS/Style.css'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { FaSearch } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
 
 function Navbar() {
     return (
         <div className='divContainer'>
             <div className='divWrapper'>
-                <ul>
-                    <li className='left'>left</li>
-                    <li className='center'>right</li>
-                    <li className='right'>center</li>
-                </ul>
+                <div className='left'>
+                    <div className='searchBox'>
+                        <input />
+                        <FaSearch />
+                    </div>
+                </div>
+                <div className='center'>
+                    <div className='navLogo'>
+                    <img src="../images/logo-new-edit-01.png"/>
+                    </div>
+                    </div>
+                <div className='right'>
+                    <div className='menuCart'>
+                    <FaShoppingCart />
+                    </div>
+                </div>
             </div>
 
         </div>
