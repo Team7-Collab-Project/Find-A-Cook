318752b1d5d049344d6863dc7ffc335f2ee2b507
Temi Owolabi <65958151+temiowolabi@users.noreply.github.com>
2023-01-25 17:59:54 +0000

    Add Functional Quantity Buttons

---
+++ main/findacook-app/src/components/Menu_Test/MenuItemList.js
@@ -0,0 +1,31 @@
+//import React from 'react';
+import React, { useState } from 'react';
+import '../CSS/Style.css'
+import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
+
+function MenuListItem() {
+
+    const [quantity, setQuantity] = useState(1);
+
+    return(
+        <>
+        <img id="cdogs" src="../images/corndogs.jpg" />
+        <div className='menu-quick'>
+            <h3>American Corndogs</h3>
+            <h5>4 Crunchy Corndogs w/ Ketchup and Mustard</h5>
+            <h6>€10</h6>
+            <div className='quantityDiv'>
+            <button className='btn-quantity-minus' onClick={() => setQuantity(quantity - 1)}>
+                <FaMinusCircle />
+            </button>
+            <p className='quantity'>{quantity}</p>
+            <button className='btn-quantity-plus' onClick={() => setQuantity(quantity + 1)}>
+                <FaPlusCircle />
+            </button>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default MenuListItem
