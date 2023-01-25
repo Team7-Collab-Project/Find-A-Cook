b45309a387b7f7955aa085b3de167d4c422e8724
Temi Owolabi <65958151+temiowolabi@users.noreply.github.com>
2023-01-25 17:59:54 +0000

    Add Functional Quantity Buttons

--- main/findacook-app/src/TestPage.js
+++ main/findacook-app/src/TestPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import MenuListItem from './components/Menu_Test/MenuItemList';
 
 function TestPage() {
     return (      
+    <>
     <Navbar />
+    <MenuListItem /></>
 ) 
   }
 
