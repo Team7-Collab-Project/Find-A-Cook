9bd836c0f5363b03dfaf053fe13520f9c94ff3c9
kamil <kamiljozefowicz@gmail.com>
2023-01-26 03:20:31 +0000

    Add image and basic styling to cook dashboard

--- main/findacook-app/src/CookDashboard.js
+++ main/findacook-app/src/CookDashboard.js
@@ -3,6 +3,20 @@ import BackButton from './components/BackButton';
 
 function CookDashboard() {
 
+
+    return (
+        <><BackButton />
+        <div id='cookdash'>
+            <div><h1>Dashboard</h1></div>
+                <div id='cookimgdiv'>
+                    <img src="./images/cookimg.jpg" alt="cook" id='cookdashimg' align='left'></img>
+                </div>
+                <div id='cookname'>
+                    Cook Name
+                </div>
+            </div>
+            </>
+    );
 }
 
 export default CookDashboard;
