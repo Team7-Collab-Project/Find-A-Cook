de31f747fcf3cfaf74cbfaaa93d16c4fc04ba9ed
kamil <kamiljozefowicz@gmail.com>
2023-01-27 03:43:53 +0000

    Add tabbed list and profile pic hover

--- main/findacook-app/src/CookDashboard.js
+++ main/findacook-app/src/CookDashboard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import BackButton from './components/BackButton';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 function CookDashboard() {
 
@@ -10,10 +12,26 @@ function CookDashboard() {
             <div><h1>Dashboard</h1></div>
                 <div id='cookimgdiv'>
                     <img src="./images/cookimg.jpg" alt="cook" id='cookdashimg' align='left'></img>
+                    <div id='profileimageoverlay'>
+                        <div id='changeprofileimagetext'>Change image</div>
+                    </div>
                 </div>
                 <div id='cookname'>
                     Cook Name
                 </div>
+                <Tabs>
+    <TabList>
+      <Tab>Orders</Tab>
+      <Tab>Menu</Tab>
+    </TabList>
+
+    <TabPanel>
+      <h2>Orders List</h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Menu list</h2>
+    </TabPanel>
+  </Tabs>
             </div>
             </>
     );
