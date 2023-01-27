8b121a22ec47816271350a2d717e4d28525f2cd2
Santos Odigie <santosodigie104@gmail.com>
2023-01-27 10:35:43 +0000

    Passwords have been encrypted using bcrypt Able to set up connection with database on aws

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mysql = require("mysql");
 const cors = require("cors");
 
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
 const app = express();
 
 app.use(express.json());
@@ -20,8 +23,42 @@ app.post('/register', (req, res)=> {
   const email = req.body.email
   const password = req.body.password
 
-  db.query("INSERT INTO users (name, email, password) VALUES (?,?,?)", [name, email, password], (err, result)=> {
-    console.log(err);
+
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+
+    if (err) {
+      console.log(err)
+    }
+
+
+    db.query("INSERT INTO users (name, email, password) VALUES (?,?,?)", [name, email, hash], (err, result)=> {
+      console.log(err);
+    });
+  })
+});
+
+app.post('/login', (req, res)=> {
+  const email = req.body.email
+  const password = req.body.password
+
+  db.query("SELECT * FROM users WHERE email = ?;", email, (err, result)=> {
+    if (err) {
+      res.send({err: err})
+    } 
+
+
+    if (result.length > 0){
+      bcrypt.compare(password, result[0].password, (error, response) => {
+        if (response) {
+          res.send(result)
+        } else {
+          res.send({ message: "Wrong username/password combination"})
+        }
+      })
+    } else {
+      res.send({ message: "User doesn't exist"})
+    }
+    
   });
 });
 
@@ -30,68 +67,3 @@ app.listen("3001", () => {
 });
 
 
-// const connection = mysql.createConnection({
-//   host: '127.0.0.1',
-//   user: 'root',
-//   password: '12345678',
-//   database: 'test_db'
-// });
-
-// router.post('/api/register', async (req, res) => {
-//     const { name, email, password } = req.body;
-
-//     try {
-//       // Hash the password
-//       const hashedPassword = await bcrypt.hash(password, 10);
-
-//       // Insert the new user into the database
-//       const query = `INSERT INTO users (name, email, password) VALUES ('${name}', '${email}', '${hashedPassword}')`;
-//       connection.query(query, (err, results) => {
-//         if (err) {
-//           return res.status(500).send(err);
-//         }
-//         res.send({ message: 'User registered successfully' });
-//       });
-//     } catch (err) {
-//       res.status(500).send(err);
-//     }
-// });
-
-// router.post('/api/login', (req, res) => {
-//     const { email, password } = req.body;
-  
-//     // Select the hashed password from the database for the entered email
-//     const query = `SELECT password FROM users WHERE email = '${email}'`;
-//     connection.query(query, (err, results) => {
-//       if (err) {
-//         return res.status(500).send(err);
-//       }
-  
-//       if (results.length > 0) {
-//         // Compare the entered password with the hashed password
-//         bcrypt.compare(password, results[0].password, (err, isMatch) => {
-//           if (err) {
-//             return res.status(500).send(err);
-//           }
-  
-//           if (isMatch) {
-//             // Password is correct
-//             // You can now create a session or a token to keep the user logged in
-//           } else {
-//             // Password is incorrect
-//             return res.status(401).send('Email or password is incorrect');
-//           }
-//         });
-//       } else {
-//         // User with the entered email does not exist
-//         return res.status(401).send('Email or password is incorrect');
-//       }
-//     });
-//   });
-
-
-// module.exports = router;
-// app.use(router);
-// app.listen(3000, () => {
-//     console.log('Server started on port 3000');
-// });
