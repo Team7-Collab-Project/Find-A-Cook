50b998d2e20b60b0feba83e73a3fc93b79b9bd29
desmondmad <desmondmad19@gmail.com>
2023-01-27 15:22:54 +0000

    Added Textfields to verifypage

--- main/findacook-app/src/VerifyPage.js
+++ main/findacook-app/src/VerifyPage.js
@@ -1,3 +1,4 @@
+import { TextField } from '@mui/material';
 import {Link} from 'react-router-dom';
 import BackButton from './components/BackButton';
 
@@ -11,8 +12,21 @@ function VerifyPage() {
         <><BackButton />
         <img id="loginLogo" src="./images/logo-new-edit-01.png"/>
         <form onSubmit={handleSubmit}>
-
-
+            <br />
+            <div id='verifyCodes'>
+                <div>
+                    <TextField className="verifyCodeArea" id="verifyCode1" />
+                </div>
+                <div>
+                    <TextField className="verifyCodeArea" id="verifyCode2" />
+                </div>
+                <div>
+                    <TextField className="verifyCodeArea" id="verifyCode3" />
+                </div>
+                <div>
+                    <TextField className="verifyCodeArea" id="verifyCode4" />
+                </div>
+            </div>
         </form>
         </>
     );
