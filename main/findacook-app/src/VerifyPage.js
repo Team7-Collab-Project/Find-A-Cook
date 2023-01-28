efd3d0d7c51281864eea9f343ac3f1e7e1414033
desmondmad <desmondmad19@gmail.com>
2023-01-28 10:34:13 +0000

    Added css and changed textfield to input

--- main/findacook-app/src/VerifyPage.js
+++ main/findacook-app/src/VerifyPage.js
@@ -1,6 +1,7 @@
 import { TextField } from '@mui/material';
 import {Link} from 'react-router-dom';
 import BackButton from './components/BackButton';
+import Button from './components/Button'
 
 const handleSubmit = (event) => {
     event.preventDefault();
@@ -11,22 +12,37 @@ function VerifyPage() {
     return (
         <><BackButton />
         <img id="loginLogo" src="./images/logo-new-edit-01.png"/>
-        <form onSubmit={handleSubmit}>
+        <form id="verifyForm" onSubmit={handleSubmit}>
+            <h1>Verification</h1>
             <br />
+            <h2>Verify your email address</h2>
+            <h3>Please check your inbox for Verification code sent to your email. Once verified, there's just one more step to complete your profile.</h3>
+            <br/>
             <div id='verifyCodes'>
                 <div>
-                    <TextField className="verifyCodeArea" id="verifyCode1" />
+                    <input id="verifyCode1" type="text" maxLength={1}/>
                 </div>
                 <div>
-                    <TextField className="verifyCodeArea" id="verifyCode2" />
+                    <input id="verifyCode2" type="text" maxLength={1}/>
                 </div>
                 <div>
-                    <TextField className="verifyCodeArea" id="verifyCode3" />
+                    <input id="verifyCode3" type="text" maxLength={1}/>
                 </div>
                 <div>
-                    <TextField className="verifyCodeArea" id="verifyCode4" />
+                    <input id="verifyCode4" type="text" maxLength={1}/>
                 </div>
             </div>
+            <br/>
+            <br />
+            <br />
+            <button>Resend Code</button>
+            <br/>
+            <br />
+            <br />
+            <button type="submit">Verify Code</button>
+            <br />
+            <br />
+            <h3>Can't find it? Please check your spam folder.</h3>
         </form>
         </>
     );
