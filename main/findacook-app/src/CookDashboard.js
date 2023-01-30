5481c894ee0afa681fe6cde8af2ff1bf00e9c2ce
kamil <kamiljozefowicz@gmail.com>
2023-01-30 18:12:18 +0000

    Add image upload

--- main/findacook-app/src/CookDashboard.js
+++ main/findacook-app/src/CookDashboard.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import BackButton from './components/BackButton';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
+
+
+
 function CookDashboard() {
 
+
+  const [selectedFile, setSelectedFile] = useState();
+    const [isFilePicked, setIsFilePicked] = useState(false);
+
+    const changeHandler = (event) => {
+		setSelectedFile(event.target.files[0]);
+	};
+
+	const handleSubmission = () => {
+        const formData = new FormData();
+
+        formData.append('File', selectedFile);
+
+        fetch(
+            'enter URL here',
+			{
+				method: 'POST',
+				body: formData,
+			}
+		)
+			.then((response) => response.json())
+			.then((result) => {
+				console.log('Success:', result);
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+			});
+	};
 
     return (
         <><BackButton />
@@ -13,7 +44,10 @@ function CookDashboard() {
                 <div id='cookimgdiv'>
                     <img src="./images/cookimg.jpg" alt="cook" id='cookdashimg' align='left'></img>
                     <div id='profileimageoverlay'>
-                        <div id='changeprofileimagetext'>Change image</div>
+                        <label class='uploadbutton' id="profilepictureupload">
+                            <input type='file' name='file' onChange={changeHandler} />
+                            <div id='changeprofileimagetext'>Change image</div>
+                        </label>
                     </div>
                 </div>
                 <div id='cookname'>
