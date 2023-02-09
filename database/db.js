3488f34c9ab6b7e253f77d223151a5de02497870
Temi Owolabi <65958151+temiowolabi@users.noreply.github.com>
2023-02-09 20:13:49 +0000

    Connected to MongoDB

---
+++ database/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(
+            'mongodb+srv://Team7:oXVVWGS8BCRZB2FM@findacook.dr9enwh.mongodb.net/?retryWrites=true&w=majority',
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
+        );
+
+        console.log('Database Connection Success');
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+module.exports = connectDB;
