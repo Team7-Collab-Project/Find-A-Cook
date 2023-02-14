0e9f6ff35689f989f0c899bc722667b5a7e56666
Temi Owolabi <65958151+temiowolabi@users.noreply.github.com>
2023-02-14 19:52:09 +0000

    Add menu model

---
+++ server/schema/MenuItemSchema.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const menus = new mongoose.Schema(
+    {
+        filename: {
+            type: String,
+            required: true,
+        },
+        // user_id: {
+        //     type: mongoose.Schema.Types.ObjectId,
+		// 	ref: 'UserSchema',
+        //     required: true,
+        // },
+        item_name: {
+            type: String,
+            required: true,
+        },
+        product_description: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        category: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'MenuCategorySchema',
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+const MenuItemSchema = mongoose.model('MenuItem', menus);
+
+module.exports = MenuItemSchema;
