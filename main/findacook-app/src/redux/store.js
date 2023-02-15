9dba4516000456974f7102eeca0c67dd137f980b
Temi Owolabi <65958151+temiowolabi@users.noreply.github.com>
2023-02-15 13:37:16 +0000

    Add reducer to store

--- main/findacook-app/src/redux/store.js
+++ main/findacook-app/src/redux/store.js
@@ -1,7 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension'
 import cartReducer from './reducers/cartReducers';
+import messageReducer from './reducers/messageReducer';
+import loadingReducer from './reducers/loadingReducer';
+import categoryReducer from './reducers/categoryReducer';
+
+
+const reducer = combineReducers({
+    messages: messageReducer,
+    loading: loadingReducer,
+    categories: categoryReducer
+})
 
 const initialState = {}
 
