ed9433500aaa316bb249b1484646d5b70575dea0
Temi Owolabi <65958151+temiowolabi@users.noreply.github.com>
2023-02-20 21:29:47 +0000

    Display Cart Items in Cart

--- main/findacook-app/src/components/Cart/Cart_test.js
+++ main/findacook-app/src/components/Cart/Cart_test.js
@@ -1,47 +1,74 @@
-import React, { useState } from 'react';
-import '../CSS/Style.css'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import "../CSS/Style.css";
 
-const Cart_test = ({cartItems = []}) => {
+const Cart_test = () => {
+  const { cart } = useSelector((state) => state.cart);
 
-    return(
-        <div className='cartContainer'>
-            <div className='cartLeftDiv'>
+  const handleGoBackBtn = () => {};
 
-                {cartItems.length === 0 && (
-                    <div className='cart-items-empty'>No items are added.</div>
-                )}
-
-                <div>
-                    {cartItems.map((item) => (
-                        <div key={item.id} className="">
-                            <img
-                            className=''
-                            src={item.image}
-                            alt={item.name}
-                            />
-                        </div>
-                    ))}
-                </div>
-
-                {/* <table className='cartTable'>
+  return (
+    <>
+      <div className="cartContainer">
+        <div className="cartLeftDiv">
+          <section>
+            {cart.length <= 0 ? (
+              <div>
+                <h1>
+                  No items added in cart.{" "}
+                  <button onClick={handleGoBackBtn}>Go Back</button>
+                </h1>
+              </div>
+            ) : (
+              <div>
+                <table className='cartTable'>
+                    <thead>
                     <tr className='cartTr'>
                     <th>Product</th>
                     <th>Name</th>
                     <th>Price</th>
                     <th>Quantity</th>
-                    <th>Total</th>
+                    <th>Remove</th>
                     </tr>
-                    <tr className='cart-tr'>
+                    </thead>
+                    <tbody>
+                        {cart.map(product => (
+                            <tr className='cart-tr'>
+                        <td>
+                            <div className='imgContainer'>
+                            <img id="cartCorndog" src={`/uploads/${product.filename}`} />
+                            </div>
+                        </td>
+                        <td>
+                            <span className='cartNameSpan'>{product.item_name}</span>
+                        </td>
+                        <td>
+                            {' '}
+                            <span className='cartPriceSpan'>{product.price.toLocaleString("en-GB", {style:"currency", currency:"EUR"})}</span>
+                        </td>
+                        <td>
+                            <span className='cartQuantitySpan'>{product.count}</span>
+                        </td>
+                        <td>
+                            {' '}
+                            <button type="button">
+                                Delete
+                            </button>
+                        </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                    {/* <tr className='cart-tr'>
                         <td>
                             <div className='imgContainer'>
                             <img id="cartCorndog" src="../images/corndogs.jpg" />
                             </div>
                         </td>
-                        <td>
+<td>
                             <span className='cartNameSpan'>Corndogs</span>
                         </td>
                         <td>
-                            <span className='cartPriceSpan'>€10</span>
+                            <span                        span className='cartPriceSpan'>€10</span>
                         </td>
                         <td>
                             <span className='cartQuantitySpan'>2</span>
@@ -49,26 +76,30 @@ const Cart_test = ({cartItems = []}) => {
                         <td>
                             <span className='cartTotalPriceSpan'>€20</span>
                         </td>
-                    </tr>
+                    </tr> */}
                 </table>
-            </div>
-            <div className='cartRight'>
-                <div className='cartWrapper'>
-                    <h2 className='cartTitle'>CART TOTAL</h2>
-                    <div className='totalText'>
-                    <b className='totalTextTitle'>Subtotal:</b>€20
-                </div>
-                <div className='totalText'>
-                    <b className='totalTextTitle'>Discount:</b>€0.00
-                </div>
-                <div className='totalText'>
+                <div className='cartRight'>
+                <div className="cartWrapper">
+                    <h2 className="cartTitle">CART TOTAL</h2>
+                    <div className="totalText">
+                    <b className="totalTextTitle">Subtotal:</b>
+                    </div>
+                    <div className="totalText">
+                    <b className='totalTextTitle'>Discount:</b>
+                    </div>
+                    <div className="totalText">
                     <b className='totalTextTitle'>Total:</b>€20
+                    </div>
+                    <button className='cartButton'>CHECKOUT NOW!</button>
                 </div>
-                <button className='cartButton'>CHECKOUT NOW!</button>
-                </div> */}
-            </div>
+                </div>
+              </div>
+            )}
+          </section>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default Cart_test
+export default Cart_test;
