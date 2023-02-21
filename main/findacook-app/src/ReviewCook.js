eb0ce96e2ffcf67602273dcc26f9af6cd07f8658
ConorMcGuire <conor@cjmcguire.net>
2023-02-21 13:47:37 +0000

    Typescript and Reactstrap libraries added

--- main/findacook-app/src/ReviewCook.js
+++ main/findacook-app/src/ReviewCook.js
@@ -16,6 +16,13 @@ function ReviewCook() {
                 Title
                 Review body
                 */}
+
+            <br></br>
+            <TextField required variant="filled" helperText="Please enter your name" className='formInput' label="Name" value={name} onChange={(event) => setName(event.target.value)} inputProps={{ pattern: "^[A-Za-z]{1,40}$" }} />
+            <br />
+            <br />
+            <TextField required variant="filled" helperText="Please enter your surname" className='formInput' label="Surname" value={surname} onChange={(event) => setSurname(event.target.value)} inputProps={{ pattern: "^[A-Za-z]{1,40}$" }} />
+            <br />
         </form>
     </div>
     )
