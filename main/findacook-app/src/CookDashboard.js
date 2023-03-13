ca49177ede4bdf2b37f8feec4a303ba3eddfef5b
desmondmad <desmondmad19@gmail.com>
2023-03-13 21:07:22 +0000

    Initial Commit - added calendar

--- main/findacook-app/src/CookDashboard.js
+++ main/findacook-app/src/CookDashboard.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React from 'react';
 //import BackButton from './components/BackButton';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
@@ -11,71 +11,16 @@ import {
   Container,
   Row,
   Col,
-  Image,
-  Button,
-  Card,
-  Badge,
-  Form,
-  FormControl,
+  Image
 } from "react-bootstrap";
+import Schedule from './components/Schedule';
+import Navbar from './components/Navbar/Navbar';
 
 
 function CookDashboard() {
-
-
-  const [selectedFile, setSelectedFile] = useState();
-  const [isFilePicked, setIsFilePicked] = useState(false);
-
-  const [isOpen, setIsOpen] = useState(false);
-  const [clicksOutside, setClicksOutside] = useState(0);
-  const navbarRef = React.useRef(null);
-
-  const handleClickOutside = event => {
-    if (isOpen && navbarRef.current && !navbarRef.current.contains(event.target)) {
-      setClicksOutside(clicksOutside + 1);
-    }
-  };
-
-  React.useEffect(() => {
-    document.addEventListener("click", handleClickOutside);
-    return () => {
-      document.removeEventListener("click", handleClickOutside);
-    };
-  });
-
-  React.useEffect(() => {
-    if (clicksOutside > 0) {
-      setIsOpen(false);
-      setClicksOutside(0);
-    }
-  }, [clicksOutside]);
-
-  const changeHandler = (event) => {
-		setSelectedFile(event.target.files[0]);
-	};
-
-	const handleSubmission = () => {
-    const formData = new FormData();
-    formData.append('File', selectedFile);
-      fetch(
-        'enter URL here',
-			  {
-				  method: 'POST',
-				  body: formData,
-			  }
-		  )
-			.then((response) => response.json())
-			.then((result) => {
-				console.log('Success:', result);
-			})
-			.catch((error) => {
-				console.error('Error:', error);
-			});
-	};
-
   return (
-    <><HamburgNavbar setIsOpen={setIsOpen}/>
-      <div id='cookdash' className={`${isOpen ? 'sidebar-open' : ''}`}>
+    <><Navbar />
+      <div id='cookdash'>
       {/*<Maps center={{ lat: 37.7749, lng: -122.4194 }} zoom={11}/>*/}
         <div><h1>Dashboard</h1></div>
         <Container id="dashboardBG">
@@ -101,37 +46,12 @@ function CookDashboard() {
 
           <Tabs>
             <TabList>
-              <Tab>Orders</Tab>
+              <Tab>Bookings</Tab>
               <Tab>Menu</Tab>
             </TabList>
             <TabPanel className='tablist'>
-              <h2>Orders List</h2>
-              <List>
-                <ListItem disablePadding className="ListItem">
-                    <ListItemButton className="ListItemButton">
-                        <table>
-                            <tr>
-                                <td><h2>John Brown</h2></td>
-                                <td><h4>259 Greenacres, Marshes Lower, Dundalk, Co. Louth, A91 D8X3</h4></td>
-                                <td><h4>Chicken Tikka Masala, Chicken Pakora</h4></td>
-                                <td><h2>24/02/22 18:00</h2></td>
-                            </tr>
-                        </table>
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding className="ListItem">
-                    <ListItemButton className="ListItemButton">
-                        <table>
-                            <tr>
-                                <td><h2>Hannah O'Reilly</h2></td>
-                                <td><h4>4 Drive 2, Muirhevnamore, Dundalk, Co. Louth, A91 P2X6</h4></td>
-                                <td><h4>Chicken Korma, Chicken Pakora</h4></td>
-                                <td><h2>27/02/22 16:30</h2></td>
-                            </tr>
-                        </table>
-                    </ListItemButton>
-                </ListItem>
-                </List>
+              <h2>Bookings</h2>
+              <Schedule />
             </TabPanel>
             <TabPanel className='tablist'>
               <h2>Menu list</h2>
@@ -140,7 +60,7 @@ function CookDashboard() {
                     <ListItemButton className="ListItemButton">
                         <table>
                             <tr>
-                                <td><img src='./images/tikka.jpg'/></td>
+                                <td><img alt="tikka" src='./images/tikka.jpg'/></td>
                                 <td><h2>Chicken Tikka Masala</h2></td>
                                 <td><h2>Main Course</h2></td>
                                 <td><h2>€29.99</h2></td>
@@ -152,7 +72,7 @@ function CookDashboard() {
                     <ListItemButton className="ListItemButton">
                         <table>
                             <tr>
-                                <td><img src='./images/korma.jpg'/></td>
+                                <td><img alt="korma" src='./images/korma.jpg'/></td>
                                 <td><h2>Chicken Korma</h2></td>
                                 <td><h2>Main Course</h2></td>
                                 <td><h2>€29.99</h2></td>
@@ -164,7 +84,7 @@ function CookDashboard() {
                     <ListItemButton className="ListItemButton">
                         <table>
                             <tr>
-                                <td><img src='./images/pakora.jpg'/></td>
+                                <td><img alt="pekora" src='./images/pakora.jpg'/></td>
                                 <td><h2>Chicken Pakora</h2></td>
                                 <td><h2>Starter</h2></td>
                                 <td><h2>€14.99</h2></td>
---
+++ main/findacook-app/src/components/Schedule.js
@@ -0,0 +1,112 @@
+import React from 'react'
+
+import {Calendar, dateFnsLocalizer} from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from 'date-fns/startOfWeek';
+import getDay from "date-fns/getDay";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-datepicker/dist/react-datepicker.css"
+import {useState, useEffect, useRef} from "react";
+import DatePicker from "react-datepicker";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+
+const locales = {
+  "en-IE": require("date-fns/locale/en-IE")
+}
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales
+})
+
+const events = [
+  {
+    title: "Brian Cullen",
+    start: new Date(2023,1,28,15,0,0),
+    end: new Date(2023,1,28,18,30,0)
+  },
+  {
+    title: "Unavailable",
+    start: new Date(2023,1,15),
+    end: new Date(2023,1,22)
+  },
+  {
+    title: "Joseph",
+    start: new Date(2023,1,24,12,0),
+    end: new Date(2023,1,24,15,0)
+  }
+]
+
+function Schedule(){
+  const [newEvent, setNewEvent] = useState({title:"", start:"",end:""})
+  const [allEvents, setAllEvents] = useState(events)
+  const [selectedDate, setSelectedDate] = useState();
+  const calendarRef = useRef(null);
+
+  function handleAddEvent() {
+    setAllEvents([...allEvents, newEvent]);
+    setNewEvent({title:"", start:"",end:""});
+  }
+  
+  
+
+  useEffect(() => {
+    const ref = calendarRef.current;
+    const listenSlotClick = (event) => {
+      const elements = document.elementsFromPoint(event.clientX, event.clientY);
+      const dayElement = elements.find((element) =>
+        element.matches(".rbc-day-bg")
+      );
+      if (dayElement) {
+        const date = new Date(dayElement.getAttribute("data-date"));
+        setSelectedDate(date);
+      }
+    };
+    if (calendarRef && ref) {
+      ref.addEventListener("click", listenSlotClick);
+      return () => {
+        ref.removeEventListener("click", listenSlotClick);
+      };
+    }
+  });
+
+  return (
+    <>
+    <div className="modal show" style={{ display: 'block', position: 'initial' }}>
+          <h2>Add new booking</h2>
+          <input type="text" 
+            placeholder='Add Title' 
+            style={{width:"20%", marginRight: "10px"}}
+            value={newEvent.title} 
+            onChange={(e) => setNewEvent({...newEvent, title: e.target.value})}
+          />
+          <DatePicker placeholderText='Start Date' style={{marginRight: "10px"}}
+          selected={selectedDate} onChange={(start) => setNewEvent({...newEvent,start})}/>
+          <DatePicker placeholderText='EndDate'
+          selected={newEvent.end} onChange={(end) => setNewEvent({...newEvent,end})}/>
+      
+          <Button variant="primary" onClick={() => {handleAddEvent();}}>Add Event</Button>
+    </div>
+    <div ref={calendarRef} style={{backgroundColor:"white", border: "black solid 2px", borderTopLeftRadius: "5%", borderTopRightRadius: "5%"}}>
+      <Calendar
+        components={{
+          dateCellWrapper: ({ children, value }) => React.cloneElement(children, { "data-date": value })
+        }}
+        localizer={localizer}
+        events={allEvents}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500, margin: "50px" }}
+        selectable={true}
+      />
+    </div>
+    </>
+  );
+}
+
+export default Schedule;
