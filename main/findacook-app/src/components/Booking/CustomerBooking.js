c30ffdf3d7d64a1f60975c0e4b0559bf69c9ffd1
Temi Owolabi <65958151+temiowolabi@users.noreply.github.com>
2023-03-18 23:30:17 +0000

    Add styling to Cook Individual Page

--- main/findacook-app/src/components/Booking/CustomerBooking.js
+++ main/findacook-app/src/components/Booking/CustomerBooking.js
@@ -21,10 +21,77 @@ const CustomerBooking = () => {
     return (
 <>
 
+
 {cook && (
-				<div className='row'>
-            {cook.cook_first_name}
-				</div>
+      <div class="test-container">
+	  <div class="profile-picture">
+		<img src={`/uploads/${cook.profile_picture}`} alt="Profile Picture" />
+	  </div>
+	  <div class="profile-info">
+		<h2>{cook.cook_first_name} {cook.cook_last_name}</h2>
+		<div class="credentials">
+			  <p className='verifiedTooltip'>Verified Cook <img src='../images/verified.png' className='verifiedImg'/><span class="tooltiptext">This cook is 100% verified by Find A Cook and meets our standards to ensure the highest quality experience for our clients.</span></p>
+			  <p className='verifiedTooltip'>Food Safety Cert <img src='../images/certification.png' className='verifiedImg'/><span class="tooltiptext">This cook maintains a certificate for food safety and sanitation to ensure ingredients, handling and preparation are in line with industry standards.</span></p> 
+			  <p><img src='../images/rating.jpg' id="rating-img"/>(67 reviews)</p>
+		  </div>
+		<p>{cook.description}</p>
+
+		<hr className="yoylo" />
+		<div className="yesOO">
+		<h3>{cook.cook_first_name}'s Popular Dishes</h3>
+		<div class="product-section">
+			  <div class="product-card">
+
+				  <img src="../images/tikka.jpg" alt="Product Image" />
+				  <div class="product-info">
+					  <h3>Product Name</h3>
+					  <p>Price: €19.99</p>
+				  </div>
+		<div class="user-pic">
+  <img src="../images/cook1.jpg" alt="User Profile Picture" />
+</div>
+			  </div>
+			  <div class="product-card">
+				  <img src="../images/pie.jpg" alt="Product Image" />
+				  <div class="product-info">
+					  <h3>Product Name</h3>
+					  <p>Price: €29.99</p>
+				  </div>
+			  </div>
+			  <div class="product-card">
+				  <img src="../images/gnocchi.jpg" alt="Product Image" />
+				  <div class="product-info">
+					  <h3>Product Name</h3>
+					  <p>Price: €39.99</p>
+				  </div>
+			  </div>
+			  <div class="product-card">
+				  <img src="../images/pakora.jpg" alt="Product Image" />
+				  <div class="product-info">
+					  <h3>Product Name</h3>
+					  <p>Price: €49.99</p>
+				  </div>
+			  </div>
+			  <div class="product-card">
+				  <img src="../images/bao.jpg" alt="Product Image" />
+				  <div class="product-info">
+					  <h3>Product Name</h3>
+					  <p>Price: €59.99</p>
+				  </div>
+			  </div>
+			  <div class="product-card">
+				  <img src="../images/burger.jpg" alt="Product Image" />
+				  <div class="product-info">
+					  <h3>Product Name</h3>
+					  <p>Price: €69.99</p>
+		  </div>
+			  </div>
+		  </div>
+	  </div>
+  </div>
+
+</div>
+
 			)}
 </>
     );
